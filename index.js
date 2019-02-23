16574ebf84411db1e0f146fa7fb34c8db122a4b4
Sathvik98 <47909915+Sathvik98@users.noreply.github.com>
2019-02-23 10:44:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, BrowserRouter as Router} from 'react-router-dom'; 
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Login from './components/login'; 
-import Hospital from './components/Hospital'; 
-import Anganwadi from './components/Anganwadi';
-import Anganwadi1st from './components/Anganwadi1st';
-import School1st from './components/School1st';
-import Complaint from './components/Complaint';
-import School from './components/School';
-import Orphanage from './components/Orphanage';
+import Hospital from './components/Hospital';
+import Login from './components/Login'; 
 
-ReactDOM.render(<School1st />, document.getElementById('root'));
+// const routing = (
+//     <Router>
+//         <div>
+//             <Route exact path="/" component={Login} />
+//             <Route path="/hospital" component={Hospital} />
+//         </div>
+//     </Router>
+// )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<Login />, document.getElementById('root'));
+
 serviceWorker.unregister();
